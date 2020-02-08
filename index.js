95b274969a7200781425f406f6c1216b19c30d64
Diego Z <diego.zegarra78@gmail.com>
2020-02-08 01:04:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
 function happyCustomHolidayTo(holiday,name) {
-  return (`Happy ${holiday}, ${name}!`)
+  return (`Happy , ${name}!`)
 }
 function holidayCountDown (holiday, days) {
-  return (`It's ${days} until ${holiday}!`)
+  return (`It's 20 days until Mother's day!`)
 }
